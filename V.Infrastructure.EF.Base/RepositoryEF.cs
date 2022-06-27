ed88566488b581998d8e0842a59ccc5f3526e7b2
FunTom <vogeltanz@utb.cz>
2022-06-27 15:53:07 +0200

    IDisposable implemented in RepositoryEF

--- V.Infrastructure.EF.Base/RepositoryEF.cs
+++ V.Infrastructure.EF.Base/RepositoryEF.cs
@@ -3,13 +3,33 @@ using V.Domain.Base.Entities.Abstraction;
 
 namespace V.Infrastructure.EF.Base
 {
-    public class RepositoryEF<T, TKey> : Repository<T, TKey> where T : class, IEntity<TKey>
+    public class RepositoryEF<T, TKey> : Repository<T, TKey> where T : class, IEntity<TKey>, IDisposable
     {
         protected DbContext dbContext;
 
         public RepositoryEF(DbContext dbContext) : base(dbContext.Set<T>())
         {
             this.dbContext = dbContext;
+        }
+
+
+
+
+        private bool _disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+                if (disposing)
+                {
+                    dbContext?.Dispose();
+                }
+            _disposed = true;
         }
     }
 }
